484752b5974856118482cd13bb0c3643d31f9b29
Ernest <soc221b.e@gmail.com>
2025-11-03 14:04:41 +0800

    feat: mui + react-hook-form

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,3 +1,15 @@
-export default function Button(props: { children: React.ReactNode }) {
-  return <button>{props.children}</button>;
+import MuiButton from "@mui/material/Button";
+
+export default function Button(props: {
+  type?: "submit" | "reset" | "button" | undefined;
+  children: React.ReactNode;
+}) {
+  return (
+    <MuiButton
+      variant="contained"
+      type={props.type ?? "submit" /* default type for button is 'submit' */}
+    >
+      {props.children}
+    </MuiButton>
+  );
 }
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,9 +1,46 @@
-export default function Input(props: {
-  type: string;
-  name: string;
+import React from "react";
+import {
+  Control,
+  FieldPath,
+  FieldValues,
+  useController,
+} from "react-hook-form";
+import TextField from "@mui/material/TextField";
+import { LabelContext } from "./Label";
+
+const emailPattern = /^[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$/;
+
+export default function Input<
+  TFieldValues extends FieldValues = FieldValues,
+  TContext = any,
+  TTransformedValues = TFieldValues
+>(props: {
+  type?: "text" | "email";
+  name: FieldPath<TFieldValues>;
   required: boolean;
+  control: Control<TFieldValues, TContext, TTransformedValues>;
 }) {
+  const label = React.useContext(LabelContext);
+
+  const { field, fieldState } = useController({
+    name: props.name,
+    control: props.control,
+    rules: {
+      required: props.required,
+      pattern: props.type === "email" ? emailPattern : undefined,
+    },
+  });
+
   return (
-    <input type={props.type} name={props.name} required={props.required} />
+    <TextField
+      label={label}
+      error={fieldState.invalid}
+      helperText={
+        { required: "Required field", pattern: "Invalid email format" }[
+          (fieldState.error?.type ?? "") as "required" | "pattern"
+        ]
+      }
+      {...field}
+    />
   );
 }
