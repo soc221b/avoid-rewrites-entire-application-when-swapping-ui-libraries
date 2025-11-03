1163ff99cf115fa62845a6213743c85f5ce40d7c
Ernest <soc221b.e@gmail.com>
2025-11-03 14:26:59 +0800

    feat: react-hook-form

--- src/SignIn.tsx
+++ src/SignIn.tsx
@@ -1,3 +1,4 @@
+import { useForm } from "react-hook-form";
 import Button from "./components/Button";
 import Input from "./components/Input";
 import Label from "./components/Label";
@@ -5,17 +6,15 @@ import Label from "./components/Label";
 export default function SignIn(props: {
   onSubmit: (data: { email: string }) => void;
 }) {
+  const { control, handleSubmit } = useForm({
+    defaultValues: { email: "" },
+  });
+
   return (
-    <form
-      onSubmit={(e) => {
-        e.preventDefault();
-        const data = new FormData(e.target as HTMLFormElement);
-        props.onSubmit({ email: data.get("email") as string });
-      }}
-    >
+    <form onSubmit={handleSubmit((data) => props.onSubmit(data))}>
       <Label>
         Email
-        <Input type="email" name="email" required />
+        <Input type="email" name="email" required control={control} />
       </Label>
 
       <Button>Submit</Button>
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,9 +1,41 @@
-export default function Input(props: {
-  type: string;
-  name: string;
+import {
+  Control,
+  FieldPath,
+  FieldValues,
+  useController,
+} from "react-hook-form";
+
+const emailPattern = /^[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$/;
+
+export default function Input<
+  TFieldValues extends FieldValues = FieldValues,
+  TContext = any,
+  TTransformedValues = TFieldValues
+>(props: {
+  type?: "text" | "email";
+  name: FieldPath<TFieldValues>;
   required: boolean;
+  control: Control<TFieldValues, TContext, TTransformedValues>;
 }) {
+  const { field, fieldState } = useController({
+    name: props.name,
+    control: props.control,
+    rules: {
+      required: props.required,
+      pattern: props.type === "email" ? emailPattern : undefined,
+    },
+  });
+
   return (
-    <input type={props.type} name={props.name} required={props.required} />
+    <>
+      <input {...field} />
+      <span style={{ color: "red" }}>
+        {
+          { required: "Required field", pattern: "Invalid email format" }[
+            (fieldState.error?.type ?? "") as "required" | "pattern"
+          ]
+        }
+      </span>
+    </>
   );
 }
